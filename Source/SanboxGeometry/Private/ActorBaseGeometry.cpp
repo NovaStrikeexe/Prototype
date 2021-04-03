8a90647f7a584e13e0fb147fe1fa22b926078e0e
Sergey_Artemiev <sergArtmiev0024@yandex.ru>
2021-04-03 13:48:55 +0500

    Work with FStrings and Createting my own operation of LOG

--- Source/SanboxGeometry/Private/ActorBaseGeometry.cpp
+++ Source/SanboxGeometry/Private/ActorBaseGeometry.cpp
@@ -2,7 +2,8 @@
 
 
 #include "ActorBaseGeometry.h"
-DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, Error, Error);
+#include "Engine/Engine.h"
+DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All, All)
 
 // Sets default values
 AActorBaseGeometry::AActorBaseGeometry()
@@ -16,6 +17,25 @@ AActorBaseGeometry::AActorBaseGeometry()
 void AActorBaseGeometry::BeginPlay()
 {
 	Super::BeginPlay();
+	FString name = "Unknown";
+	
+
+	int weaponsNum = 4;
+	float health = 30.5f;
+	bool isDead = false;
+	FString weponsNumStr = "Weapons number: " + FString::FromInt(weaponsNum);
+
+	FString heathFloatStr = "Health: " + FString::SanitizeFloat(health);
+
+	FString isDeadStr = "Is dead? " + FString(isDead? "true" : "false");
+
+	FString Stat = FString::Printf(TEXT(" \n=== All Stat === \n %s \n %s \n %s"),*weponsNumStr, *heathFloatStr,*isDeadStr);
+	UE_LOG(LogBaseGeometry, Display, TEXT("Name: %s"), *name);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("%s"),*Stat);
+
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f,FColor::Red, name);
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, Stat,true,FVector2D(1.5f, 1.5f));
+
 	//printTypes();
 }
 
@@ -28,14 +48,10 @@ void AActorBaseGeometry::Tick(float DeltaTime)
 
 void AActorBaseGeometry::printTypes()
 {
-	int weaponsNum = 4;
-	int killNum = 0;
-	float health = 30.5f;
-	bool isDead = false;
-	bool hasWeapon = true;
-	UE_LOG(LogTemp, Display, TEXT("Wepons number: %d, kills: num %i"), weaponsNum, killNum);
-	UE_LOG(LogTemp, Display, TEXT("Health: %.2F"), health);
-	UE_LOG(LogTemp, Display, TEXT("Is dead?: %d"), isDead);
-	UE_LOG(LogTemp, Display, TEXT("Has weapon?: %d"), static_cast<int>(hasWeapon));
+	
+	//UE_LOG(LogTemp, Display, TEXT("Wepons number: %d, kills: num %i"), weaponsNum, killNum);
+	//UE_LOG(LogTemp, Display, TEXT("Health: %.2F"), health);
+	//UE_LOG(LogTemp, Display, TEXT("Is dead?: %d"), isDead);
+	//UE_LOG(LogTemp, Display, TEXT("Has weapon?: %d"), static_cast<int>(hasWeapon));
 }
 
