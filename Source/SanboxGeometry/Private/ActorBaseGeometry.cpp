d486a3aeccefacdc09be84a168eeaebbc4ccc2d4
Sergey_Artemiev <sergArtmiev0024@yandex.ru>
2021-04-14 21:12:54 +0500

    learn with UPROPERTY

--- Source/SanboxGeometry/Private/ActorBaseGeometry.cpp
+++ Source/SanboxGeometry/Private/ActorBaseGeometry.cpp
@@ -17,26 +17,8 @@ AActorBaseGeometry::AActorBaseGeometry()
 void AActorBaseGeometry::BeginPlay()
 {
 	Super::BeginPlay();
-	FString name = "Unknown";
-	
-
-	int weaponsNum = 4;
-	float health = 30.5f;
-	bool isDead = false;
-	FString weponsNumStr = "Weapons number: " + FString::FromInt(weaponsNum);
-
-	FString heathFloatStr = "Health: " + FString::SanitizeFloat(health);
-
-	FString isDeadStr = "Is dead? " + FString(isDead? "true" : "false");
-
-	FString Stat = FString::Printf(TEXT(" \n=== All Stat === \n %s \n %s \n %s"),*weponsNumStr, *heathFloatStr,*isDeadStr);
-	UE_LOG(LogBaseGeometry, Display, TEXT("Name: %s"), *name);
-	UE_LOG(LogBaseGeometry, Warning, TEXT("%s"),*Stat);
-
-	GEngine->AddOnScreenDebugMessage(-1, 10.0f,FColor::Red, name);
-	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, Stat,true,FVector2D(1.5f, 1.5f));
-
-	//printTypes();
+	printTypes();
+	//printStringTypes();
 }
 
 // Called every frame
@@ -48,10 +30,28 @@ void AActorBaseGeometry::Tick(float DeltaTime)
 
 void AActorBaseGeometry::printTypes()
 {
-	
-	//UE_LOG(LogTemp, Display, TEXT("Wepons number: %d, kills: num %i"), weaponsNum, killNum);
-	//UE_LOG(LogTemp, Display, TEXT("Health: %.2F"), health);
-	//UE_LOG(LogTemp, Display, TEXT("Is dead?: %d"), isDead);
-	//UE_LOG(LogTemp, Display, TEXT("Has weapon?: %d"), static_cast<int>(hasWeapon));
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Actor name: %s"),*GetName());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Wepons number: %d, kills: num %i"), weaponsNum, killNum);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Is dead?: %d"), isDead);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Has weapon?: %d"), static_cast<int>(hasWeapon));
+
+}
+
+void AActorBaseGeometry::printStringTypes()
+{
+	FString name = "Unknown";
+
+	FString weponsNumStr = "Weapons number: " + FString::FromInt(weaponsNum);
+
+	FString heathFloatStr = "Health: " + FString::SanitizeFloat(health);
+
+	FString isDeadStr = "Is dead? " + FString(isDead ? "true" : "false");
+
+	FString Stat = FString::Printf(TEXT(" \n=== All Stat === \n %s \n %s \n %s"), *weponsNumStr, *heathFloatStr, *isDeadStr);
+	UE_LOG(LogBaseGeometry, Display, TEXT("Name: %s"), *name);
+	UE_LOG(LogBaseGeometry, Warning, TEXT("%s"), *Stat);
+
+	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, name);
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, Stat, true, FVector2D(1.5f, 1.5f));
 }
 
--- Source/SanboxGeometry/Public/ActorBaseGeometry.h
+++ Source/SanboxGeometry/Public/ActorBaseGeometry.h
@@ -19,10 +19,26 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, Category = "Weapon")
+	int32 weaponsNum = 4;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Stat")
+	int32 killNum = 7;
+
+	UPROPERTY(EditInstanceOnly, Category = "Health")
+	float health = 30.5f;
+
+	UPROPERTY(EditAnywhere, Category = "Health")
+	bool isDead = false;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon")
+	bool hasWeapon = true;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 private:
 	void printTypes();
+	void printStringTypes();
 };
